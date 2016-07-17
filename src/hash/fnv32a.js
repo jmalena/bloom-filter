ef3bfa294d979629a42c0f3f8d24bc29e1a1ee25
jmalena <jonasmalena@Clark-Nova.local>
2016-07-17 22:46:34 +0200

    get rid of murmurhash-native
    Replace MurmurHash with FNV-1 and FNV-1a hashs. Use double hashing to produce any number of new hash functions.

---
+++ src/hash/fnv32a.js
@@ -0,0 +1,12 @@
+/* @flow */
+
+export default function (str: string) {
+  let hash = 0x811c9dc5
+
+  for (let i = 0; i < str.length; i++) {
+    hash ^= str.charCodeAt(i)
+    hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24)
+  }
+
+  return hash >>> 0
+}
